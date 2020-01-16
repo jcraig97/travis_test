97b104a5ac2c3de4dc4f92ceaa70cf00e13f8b37
Jacob Craig <jacobcraig@kubrickgroup.com>
2020-01-16 14:30:56 +0000

    first push of the travis test

---
+++ script.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = r'https://www.bbc.co.uk/news'
+r = requests.get(url)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+title = soup.find('title')
+print(title)
